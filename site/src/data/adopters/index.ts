b887843b24e69be6616e96365d8ed295201001e1
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-12-16 14:25:12 -0500

    update adopters
    Signed-off-by: Erica Hughberg <erica.sundberg.90@gmail.com>

--- site/src/data/adopters/index.ts
+++ site/src/data/adopters/index.ts
@@ -1,5 +1,3 @@
-import adoptersData from './adopters.json';
-
 export type Adopter = {
   name: string;
   logoUrl: string;
@@ -7,8 +5,20 @@ export type Adopter = {
   description?: string;
 };
 
-// Import adopters from the consolidated JSON file
-export const adopters: Adopter[] = adoptersData as Adopter[];
+// Dynamically import all adopter JSON files (except template and README)
+// Using require.context for Docusaurus/Webpack compatibility
+const adopterFiles = (require as any).context(
+  './',
+  false,
+  /^\.\/(?!_template|adopters)[^\/]+\.json$/
+);
+
+// Load all adopters from individual JSON files
+export const adopters: Adopter[] = adopterFiles.keys().map((fileName: string) => {
+  return adopterFiles(fileName) as Adopter;
+});
 
 // Sort adopters alphabetically by name
-export const sortedAdopters: Adopter[] = [...adopters].sort((a, b) => a.name.localeCompare(b.name));
+export const sortedAdopters: Adopter[] = [...adopters].sort((a, b) =>
+  a.name.localeCompare(b.name)
+);
