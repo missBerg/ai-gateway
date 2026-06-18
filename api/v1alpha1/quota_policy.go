1d2f496b999ea474ab619b92afe4bcefe7c9ab90
Aaron Choo <achoo30@bloomberg.net>
2026-06-18 11:16:46 -0400

    api: mark ServiceQuota as todo (#2249)
    **Description**
    ServiceQuota currently needs to be implemented. The extension server
    does not inject the descriptor set but the rate limit config is properly
    configured. Adding it as a TODO/follow up.
    
    Signed-off-by: achoo30 <achoo30@bloomberg.net>

--- api/v1alpha1/quota_policy.go
+++ api/v1alpha1/quota_policy.go
@@ -40,6 +40,9 @@ type QuotaPolicySpec struct {
 	// Quota for all models served by AIServiceBackend(s). This value can be overridden for specific models using the "PerModelQuotas"
 	// configuration.
 	//
+	// Currently, the rate limit configuration is properly set up, but the descriptor set is not being set in the Envoy Configuration
+	// TODO: Add changes in the extension server to support ServiceQuota enforcement.
+	//
 	// +optional
 	ServiceQuota ServiceQuotaDefinition `json:"serviceQuota,omitempty"`
 	// PerModelQuotas specifies quota for different models served by the AIServiceBackend(s) where this
